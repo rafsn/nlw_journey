0b68b0fec58147f93ddc174e43daa6cabfce584f
rafsn <santos_rafa1973@hotmail.com>
2024-07-10 11:00:28 -0300

    #nlw_ add fonts & loading

--- nlw_journey/src/app/index.tsx
+++ nlw_journey/src/app/index.tsx
@@ -1,9 +1,9 @@
 import {View, Text} from "react-native";
 
-export default function Index(){
+export default function Index() {
     return (
-        <View>
-            <Text>Olá, Nlw</Text>
+        <View className="flex-1 items-center justify-center">
+            <Text className="text-zinc-400">Olá, Nlw</Text>
         </View>
     )
 }
